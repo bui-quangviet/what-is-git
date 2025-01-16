50b1e7608b7a7b88cf3606054d6f33178a51e0f6
diepnhi <nguyendiepnhi@jennie-2.local>
2025-01-16 16:23:14 +0700

    new commit/dnhi

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const numberDisplay = document.getElementById('number-display');
   const incrementButton = document.getElementById('increment-button');
 
-  let number = 0;
+  let number = 1;
 
   incrementButton.addEventListener('click', () => {
       number++;
       numberDisplay.textContent = number;
   });
 });
+
